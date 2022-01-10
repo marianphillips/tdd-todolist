7633b1f79de72d7c59d24f994d82fec4c048d6ad
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-01-10 17:49:19 +0000

    updated task

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -7,16 +7,169 @@ describe("TodoList", () => {
     todoList = new TodoList()
   })
 
+  
   it("creates a todo item", () => {
     // set up
-    const expected = {
+    const expected = [{
       id: 1,
       text: "turn the heating on!",
       status: "incomplete"
-    }
+    }]
     // execute
     const result = todoList.create("turn the heating on!")
     // verify
-    expect(result).toEqual(expected)
+    expect(todoList.list).toEqual(expected)
   })
+
+  it("creates a todo list with 2 items", () => {
+    // set up
+    const expected = [{
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    }, {
+      id: 2,
+      text: "wash the sheets!",
+      status: "incomplete"
+    }]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    // verify
+    expect(todoList.list).toEqual(expected)
+  })
+
+
+  it("getAll to return full to do", () => {
+    // set up
+    const expected = [{
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete"
+    }, {
+      id: 2,
+      text: "wash the sheets!",
+      status: "incomplete"
+    }]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    // verify
+    expect(todoList.getAll()).toEqual(expected)
+  })
+
+  it("set to complete", () => {
+    // set up
+    const expected = [{
+      id: 1,
+      text: "turn the heating on!",
+      status: "complete"
+    }, {
+      id: 2,
+      text: "wash the sheets!",
+      status: "incomplete"
+    }]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.setComplete(1)
+    // verify
+    expect(todoList.getAll()).toEqual(expected)
+  })
+
+  it("get complete", () => {
+    // set up
+    const expected = [ {
+      id: 1,
+      text: "turn the heating on!",
+      status: "complete"
+    },
+]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.create("make dinner!")
+    todoList.setComplete(1)
+    // verify
+    expect(todoList.getComplete()).toEqual(expected)
+  })
+
+  it("get incomplete", () => {
+    // set up
+    const expected = [ {
+      id: 2,
+      text: "wash the sheets!",
+      status: "incomplete"
+    },
+    {
+      id: 3,
+      text: "make dinner!",
+      status: "incomplete"
+    },
+]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.create("make dinner!")
+    todoList.setComplete(1)
+    // verify
+    expect(todoList.getIncomplete()).toEqual(expected)
+  })
+
+  it("search by ID and return", () => {
+    // set up
+    const expected =  {
+      id: 3,
+      text: "make dinner!",
+      status: "incomplete"
+    }
+
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.create("make dinner!")
+    todoList.setComplete(1)
+    // verify
+    expect(todoList.searchByID(3)).toEqual(expected)
+  })
+
+  it("search by ID and return, ID does not exit", () => {
+    // set up
+    const expected = "NOOOOOOOOOO"
+
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.create("make dinner!")
+    todoList.setComplete(1)
+    // verify
+    expect(todoList.searchByID(4)).toEqual(expected)
+  })
+
+  it("search by ID and remove", () => {
+    // set up
+    const expected = [ {
+      id: 1,
+      text: "turn the heating on!",
+      status: "complete"
+    },
+    {
+      id: 2,
+      text: "wash the sheets!",
+      status: "incomplete"
+    },
+]
+
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("wash the sheets!")
+    todoList.create("make dinner!")
+    todoList.setComplete(1)
+    todoList.removeByID(3)
+
+    // verify
+    expect(todoList.list).toEqual(expected)
+  })
+
+
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,66 @@
+
 class TodoList {
+ constructor() {
+     this.list = []
+     this.counter = 1
+ }
+
+ create(listItem) {
+    let listObject = {id:this.counter, text: listItem, status: "incomplete"}
+     this.list.push(listObject)
+     this.counter++
+ }
+
+ getAll() {
+    return this.list
+ }
+
+setComplete(listID) {
+for(let i = 0; i < this.list.length; i++) {
+    if(this.list[i].id === listID) {
+        this.list[i].status = "complete"
+    }
+}
+}
+
+getIncomplete() {
+    let incomplete = []
+    for(let i = 0; i < this.list.length; i++) {
+        if(this.list[i].status === "incomplete") {
+            incomplete.push(this.list[i])
+        }
+    }
+    return incomplete
+}
+
+getComplete() {
+    let complete = []
+    for(let i = 0; i < this.list.length; i++) {
+        if(this.list[i].status === "complete") {
+            complete.push(this.list[i])
+        }
+    }
+    return complete
+}
+
+
+searchByID(listID) {
+ for(let i = 0; i<this.list.length; i++) {
+     if(this.list[i].id === listID) {
+         return this.list[i]
+     }
+ }
+ return "NOOOOOOOOOO"
+}
+
+removeByID(listID) {
+    for(let i = 0; i<this.list.length; i++) {
+        if(this.list[i].id === listID) {
+           this.list.splice(i,1)
+        }
+    }
+}
+
 
 }
 
